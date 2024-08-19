083b1eb88ee30ba9a745fbf6f5aa31a47655b94a
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2024-08-19 16:14:19 +0530

    Update ubuntu-jenkins.sh

--- ubuntu-jenkins.sh
+++ ubuntu-jenkins.sh
@@ -2,8 +2,10 @@ sudo apt-get update
 sudo apt install git maven -y
 wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
 echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
-sudo apt-get update; sudo apt-get install -y java-1.8.0-amazon-corretto-jdk
-sudo apt-get install java-17-amazon-corretto-jdk -y
+sudo apt-get update; sudo apt-get install -y java-1.8.0-amazon-corretto-jdk -y
+wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
+sudo apt-get update; sudo apt-get install -y java-17-amazon-corretto-jdk -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
