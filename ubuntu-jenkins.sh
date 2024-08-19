7729e918a50a1f441e95cc211ef6d3509f252029
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2024-08-19 15:38:34 +0530

    Update ubuntu-jenkins.sh

--- ubuntu-jenkins.sh
+++ ubuntu-jenkins.sh
@@ -1,11 +1,12 @@
 sudo apt-get update
-sudo apt-get install openjdk-11-jdk
+sudo apt install git java-1.8.0-openjdk maven -y
+sudo apt-get install -y java-17-amazon-corretto-jdk -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
+update-alternative --config java
 sudo systemctl start jenkins.service
 sudo systemctl status jenkins
