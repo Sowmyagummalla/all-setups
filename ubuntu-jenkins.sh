93fdaabac5b6bbd391612c73321b7616c22db3df
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2024-08-19 19:55:03 +0530

    Update ubuntu-jenkins.sh

--- ubuntu-jenkins.sh
+++ ubuntu-jenkins.sh
@@ -9,4 +9,6 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
   sudo apt-get install fontconfig openjdk-17-jre
+  update-alternative --config java
   sudo apt-get install jenkins
+  systemctl status jenkins
