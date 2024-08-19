bc83bfef636f89b9a78494b145f59f9627fd7f2c
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2024-08-19 19:49:05 +0530

    Update ubuntu-jenkins.sh

--- ubuntu-jenkins.sh
+++ ubuntu-jenkins.sh
@@ -3,15 +3,10 @@ sudo apt install git maven -y
 wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
 echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
 sudo apt-get update; sudo apt-get install -y java-1.8.0-amazon-corretto-jdk -y
-wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
-echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
-sudo apt-get update; sudo apt-get install -y java-17-amazon-corretto-jdk -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get install jenkins -y
-update-alternative --config java
-sudo systemctl start jenkins.service
-sudo systemctl status jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+  sudo apt-get install fontconfig openjdk-17-jre
+  sudo apt-get install jenkins
