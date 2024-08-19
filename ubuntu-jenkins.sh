e122cb8819c96199d255d511be107ee046adcc9c
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2024-08-19 20:01:47 +0530

    Update ubuntu-jenkins.sh

--- ubuntu-jenkins.sh
+++ ubuntu-jenkins.sh
@@ -1,14 +1,10 @@
 sudo apt-get update
 sudo apt install git maven -y
-wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
-echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
-sudo apt-get update; sudo apt-get install -y java-1.8.0-amazon-corretto-jdk -y
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
     https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
-  sudo apt-get install fontconfig openjdk-17-jre
-  update-alternatives --config java
+  sudo apt-get install fontconfig openjdk-17-jre -y
   sudo apt-get install jenkins
   systemctl status jenkins
